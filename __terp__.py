3e8c225fab9e9c9e5ae1af308d928f644b73933f
Eduard Carreras <ecarreras@gmail.com>
2018-01-05 08:33:41 +0100

    Bump to v0.20.0

--- __terp__.py
+++ __terp__.py
@@ -25,7 +25,7 @@
 
 {
     "name" : "Powerful Email capabilities for Open ERP",
-    "version" : "0.19.3",
+    "version" : "0.20.0",
     "author" : "Sharoon Thomas, Openlabs",
     "website" : "http://openlabs.co.in/blog/post/poweremail/",
     "category" : "Added functionality",
