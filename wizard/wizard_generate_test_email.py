b3913aaf83abd5cddcd06f75ca147af53a328f11
glloveras <glloveras@gisce.net>
2022-09-06 15:57:50 +0200

    add register when create test email

--- wizard/wizard_generate_test_email.py
+++ wizard/wizard_generate_test_email.py
@@ -3,7 +3,7 @@
 from osv import osv, fields
 from tools.translate import _
 import tools
-
+from poweremail.poweremail_template import get_value
 
 class WizardGenerateTestEmail(osv.osv_memory):
 
@@ -19,6 +19,15 @@ class WizardGenerateTestEmail(osv.osv_memory):
         wizard = self.browse(cursor, uid, ids[0], context=context)
         template_ids = context['active_ids']
 
+        if not wizard.model_ref:
+            raise osv.except_osv(
+                _('Error'),
+                _('No model reference defined')
+            )
+        model_ref = wizard.model_ref.split(',')
+        model_name = model_ref[0]
+        model_id = int(model_ref[1])
+
         if not isinstance(template_ids, (list, tuple)):
             template_ids = [template_ids]
 
@@ -32,6 +41,16 @@ class WizardGenerateTestEmail(osv.osv_memory):
                 cursor, uid, template.id, context=context
             )
 
+            # Evaluates an expression and returns its value
+            # recid: ID of the target record under evaluation
+            # message: The expression to be evaluated
+            # template: BrowseRecord object of the current template
+            # return: Computed message (unicode) or u""
+            body_text = get_value(
+                cursor, uid, model_id, message=template.def_body_text,
+                template=template, context=context
+            )
+
             mail_vals = {
                 'pem_from': tools.ustr(from_account['name']) + \
                             "<" + tools.ustr(from_account['email_id']) + ">",
@@ -39,7 +58,7 @@ class WizardGenerateTestEmail(osv.osv_memory):
                 'pem_cc': False,
                 'pem_bcc': False,
                 'pem_subject': template.name,
-                'pem_body_text': template.def_body_text,
+                'pem_body_text': body_text,
                 'pem_account_id': from_account['id'],
                 'priority': '1',
                 'state': 'na',
@@ -59,9 +78,31 @@ class WizardGenerateTestEmail(osv.osv_memory):
             'res_model': 'poweremail.mailbox',
             'type': 'ir.actions.act_window'
         }
+
+    def _ref_models(self, cursor, uid, context=None):
+        template_obj = self.pool.get('poweremail.templates')
+
+        if not context:
+            context = {}
+
+        template_ids = context['active_ids']
+
+        if not isinstance(template_ids, (list, tuple)):
+            template_ids = [template_ids]
+        res = []
+        for template in template_obj.browse(cursor, uid, template_ids, context=context):
+            template_model = template.object_name.model
+            template_name = template.object_name.name
+            res.append((template_model, template_name))
+
+        return res
     
     _columns = {
         'state': fields.selection([('init', 'Init'), ('end', 'End')], 'State'),
+        'model_ref': fields.reference(
+            "Template reference", selection=_ref_models,
+            size=64, required=True
+        ),
     }
 
     _defaults = {
