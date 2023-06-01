bba199c25fea287a617264ad4b4816c13458f205
Gerard Parareda <gparareda@gisce.net>
2023-06-01 15:24:40 +0200

    Fix missing comma in update __terp__

--- __terp__.py
+++ __terp__.py
@@ -62,7 +62,7 @@
         'poweremail_send_wizard.xml',
         'poweremail_mailbox_view.xml',
         'poweremail_serveraction_view.xml',
-        'wizard/wizard_emails_generats.xml'
+        'wizard/wizard_emails_generats.xml',
         'wizard/wizard_state_poweremail.xml',
         'wizard/wizard_send_email.xml',
         'wizard/wizard_poweremail.xml'
