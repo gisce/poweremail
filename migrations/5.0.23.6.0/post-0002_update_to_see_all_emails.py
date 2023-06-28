58b8aaa7bc9f105949b715daf907e3919781b6e2
DejamePonermeElNombre <ralumi2002@gmail.com>
2023-06-28 15:08:55 +0200

    Added migration scprit and translation files

---
+++ migrations/5.0.23.6.0/post-0002_update_to_see_all_emails.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import logging
+from oopgrade.oopgrade import load_data_records
+
+
+def up(cursor, installed_version):
+    if not installed_version:
+        return
+
+    logger = logging.getLogger('openerp.migration')
+
+    logger.info('Adding the menu to see all the emails')
+
+    view = "poweremail_mailbox_view.xml"
+    view_record = [
+        "poweremail_all_emails_tree",
+        "action_poweremail_all_emails_tree",
+        "action_poweremail_all_emails_tree_company",
+    ]
+    logger.info("Updating XML {}".format(view))
+    load_data_records(
+        cursor, 'poweremail', view, view_record, mode='update'
+    )
+    logger.info('XML successfully updated.')
+
+
+def down(cursor, installed_version):
+    pass
+
+
+migrate = up
