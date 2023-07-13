033e00614f1a991812b84786440a042f7ad692b7
Joan <jperez@gisce.net>
2023-07-13 14:21:55 +0200

    Fix migration script

--- migrations/5.0.23.9.0/post-0002_add_template_id_mailbox_view.py
+++ migrations/5.0.23.9.0/post-0002_add_template_id_mailbox_view.py
@@ -1,5 +1,7 @@
+# -*- encoding: utf-8 -*-
 import logging
-from oopgrade.oopgrade import load_data_records
+import pooler
+from oopgrade.oopgrade import load_data, load_data_records
 
 
 def up(cursor, installed_version):
@@ -7,12 +9,25 @@ def up(cursor, installed_version):
         return
 
     logger = logging.getLogger('openerp.migration')
-    logger.info('Updating account mailbox view (poweremail_mailbox_form)...')
+
+    logger.info("Creating pooler")
+    pool = pooler.get_pool(cursor.dbname)
+
+    ##UPDATAR UN MODUL NOU AL CREAR-LO O AFEGIR UNA COLUMNA##
+    logger.info("Creating table: poweremail.mailbox")
+    pool.get("poweremail.mailbox")._auto_init(cursor,context={'module': 'poweremail'})
+    logger.info("Table created succesfully.")
+
+    ##UPDATAR UNA PART DE L'XML (POSAR LA ID)##
+    logger.info("Updating XMLs")
+    list_of_records = [
+        "poweremail_mailbox_form"
+    ]
     load_data_records(
-        cursor, 'poweremail',
-        'poweremail_mailbox_view.xml', ['poweremail_mailbox_form']
+        cursor, 'poweremail', 'poweremail_mailbox_view.xml',
+        list_of_records, mode='update'
     )
-    logger.info('View updated (poweremail_mailbox_form)!')
+    logger.info("XMLs succesfully updated.")
 
 
 def down(cursor, installed_version):
