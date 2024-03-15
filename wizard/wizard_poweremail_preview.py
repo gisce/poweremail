57bd8f7110cb4785937a12962080f7c19fbfe5f8
Demo Name <mserrano@gisce.net>
2024-03-15 09:32:43 +0100

    Fix code

--- wizard/wizard_poweremail_preview.py
+++ wizard/wizard_poweremail_preview.py
@@ -51,36 +51,42 @@ class poweremail_preview(osv.osv_memory):
     }
 
     def action_generate_static_mail(self, cr, uid, ids, context=None):
-        model_ref = self.read(cr, uid, ids, ['model_ref'], context)
+        wizard_values = self.read(cr, uid, ids, ['model_ref'], context=context)
+
         if context is None:
             context = {}
-        if not model_ref:
+        if not wizard_values:
             return {}
+
         vals = {}
-        model_name, record_id = model_ref[0]['model_ref'].split(',')
+        model_name, record_id = wizard_values[0]['model_ref'].split(',')
         record_id = int(record_id)
         template = self.pool.get('poweremail.templates').browse(cr, uid, context['active_id'], context=context)
         # Search translated template
         lang = get_value(cr, uid, record_id, template.lang, template, context)
         ctx = context.copy()
+
         if lang:
-            ctx.update({'lang':lang})
+            ctx.update({'lang': lang})
             template = self.pool.get('poweremail.templates').browse(cr, uid, context['active_id'], ctx)
-        vals['to'] = get_value(cr, uid, record_id, template.def_to, template, ctx)
-        vals['cc'] = get_value(cr, uid, record_id, template.def_cc, template, ctx)
-        vals['bcc'] = get_value(cr, uid, record_id, template.def_bcc, template, ctx)
-        vals['subject'] = get_value(cr, uid, record_id, template.def_subject, template, ctx)
+
+        mail_fields = ['to', 'cc', 'bcc', 'subject', 'body_text', 'body_html', 'report']
         ctx.update({'raise_exception': True})
-        try:
-            vals['body_text'] = get_value(cr, uid, record_id, template.def_body_text, template, ctx)
-        except Exception as e:
-            import traceback
-            tb=traceback.format_tb(sys.exc_info()[2])
-            vals['body_text'] = ''.join(tb)
-            vals['state'] = 'error'
-        vals['body_html'] = get_value(cr, uid, record_id, template.def_body_html, template, ctx)
-        vals['report'] = get_value(cr, uid, record_id, template.file_name, template, ctx)
+        for field in mail_fields:
+            try:
+                if field == 'report':
+                    field_value = template.file_name
+                else:
+                    field_value = getattr(template, "def_{}".format(field))
+                vals[field] = get_value(cr, uid, record_id, field_value, template, ctx)
+            except Exception as e:
+                import traceback
+                tb = traceback.format_tb(sys.exc_info()[2])
+                vals[field] = ''.join(tb)
+                vals['state'] = 'error'
+
         self.write(cr, uid, ids, vals, context=context)
+        return True
 
 
     def action_send_static_mail(self, cursor, uid, ids, context=None):
@@ -98,9 +104,8 @@ class poweremail_preview(osv.osv_memory):
                 _('Error'),
                 _('No model reference defined')
             )
-        model_ref = wizard.model_ref.split(',')
-        model_name = model_ref[0]
-        model_id = int(model_ref[1])
+        model_name, model_id = wizard.model_ref.split(',')
+        model_id = int(model_id)
 
         if not isinstance(template_ids, (list, tuple)):
             template_ids = [template_ids]
