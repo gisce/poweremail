aaae654871bc39ab6386957f69098f54d90d2f80
Ronaldo Cuentas Alave <rcuentas@gisce.net>
2024-03-20 10:30:09 +0100

    add auto_init

--- migrations/5.0.23.12.0/post-0002_update_poweremail_preview_view.py
+++ migrations/5.0.23.12.0/post-0002_update_poweremail_preview_view.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 import logging
+import pooler
 from oopgrade.oopgrade import load_data, load_data_records
 
 
 def up(cursor, installed_version):
     if not installed_version:
         return
-
+    pool = pooler.get_pool(cursor.dbname)
     logger = logging.getLogger('openerp.migration')
 
-
+    pool.get("poweremail.preview")._auto_init(cursor, context={'module': 'poweremail'})
     logger.info("Updating XML wizard_poweremail_preview.xml")
     load_data_records(
         cursor, 'poweremail', 'wizard/wizard_poweremail_preview.xml', ['poweremail_preview_form'], mode='update')
