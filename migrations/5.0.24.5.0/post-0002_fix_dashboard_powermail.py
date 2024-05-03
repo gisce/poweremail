5d1d3c271bee11c64066f5aed5d6703c72883a70
aoliva <aoliva@gisce.net>
2024-05-03 14:57:50 +0200

    Fix dashboard i script migració modificacio

---
+++ migrations/5.0.24.5.0/post-0002_fix_dashboard_powermail.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import logging
+from oopgrade.oopgrade import load_data, load_data_records
+
+
+def up(cursor, installed_version):
+    if not installed_version:
+        return
+
+
+    logger = logging.getLogger('openerp.migration')
+
+    logger.info("Updating XML's")
+    list_of_records = [
+        "view_poweremail_enviats_avui",
+        "action_poweremail_enviats_avui",
+        "action_poweremail_emails_rebuts_avui",
+        "view_poweremail_emails_ultims_60_dies",
+        "action_poweremail_emails_ultims_60_dies"
+    ]
+    load_data_records(
+        cursor, 'poweremail', 'poweremail_dashboard.xml', list_of_records, mode='update'
+    )
+    logger.info("XMLs succesfully updated.")
+
+def down(cursor, installed_version):
+    pass
+
+migrate = up
