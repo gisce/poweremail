d0c8002457055e1d79a47d5caae5091a6219f399
saul-timoteo <saul.timoteo022@gmail.com>
2024-10-30 11:58:10 +0100

    feat: add menuitem and migration script

---
+++ migrations/5.0.25.1.0/post-0001_add_menu_item_for_sending_mail_state.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from tools import config
+from oopgrade.oopgrade import load_data_records
+
+
+def up(cursor, installed_version):
+    if not installed_version or config.updating_all:
+        return
+    load_data_records(
+        cursor, 'poweremail', "poweremail_mailbox_view.xml", [
+        "action_poweremail_sending_tree",
+        "action_poweremail_sending_tree_company",
+        "menu_action_poweremail_sending_tree_company",
+        "menu_action_poweremail_sending_tree"
+    ], mode='update')
+
+
+def down(cursor, installed_version):
+    pass
+
+
+migrate = up
