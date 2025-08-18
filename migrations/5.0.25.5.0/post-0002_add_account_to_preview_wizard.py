d580799c796b9e1c5aebb5797bf2f222b69b956e
Yisus <sgutierrez@gisce.net>
2025-08-18 09:30:28 +0200

    Feat: Adding translations

--- migrations/5.0.25.5.0/post-0002_add_account_to_preview_wizard.py
+++ migrations/5.0.25.5.0/post-0002_add_account_to_preview_wizard.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from oopgrade.oopgrade import load_data_records
+from tools.translate import trans_load
 from tools import config
-import pooler
 
 
 def up(cursor, installed_version):
@@ -16,6 +16,9 @@ def up(cursor, installed_version):
     load_data_records(
         cursor, module_name, 'wizard/wizard_poweremail_preview.xml',
         list_of_records, mode='update')
+
+    trans_load(cursor, '{}/poweremail/i18n/ca_ES.po'.format(config['addons_path']), 'ca_ES')
+    trans_load(cursor, '{}/poweremail/i18n/es_ES.po'.format(config['addons_path']), 'es_ES')
 
 
 def down(cursor, installed_version):
