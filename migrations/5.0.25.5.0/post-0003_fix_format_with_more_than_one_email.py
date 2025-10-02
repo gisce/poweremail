5013cb44856f7bdb8c3a22b3ac772a04cf71e297
Ronaldo Cuentas Alave <rcuentas@gisce.net>
2025-10-02 12:48:39 +0200

    Modificar el formato del correo en las direcciones

---
+++ migrations/5.0.25.5.0/post-0003_fix_format_with_more_than_one_email.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from tools import config
+
+
+def up(cursor, installed_version):
+    if not installed_version or config.updating_all:
+        return
+
+    cursor.execute("UPDATE res_partner_address SET email = REPLACE(email, ';', ',') WHERE email LIKE '%;%';")
+
+def down(cursor, installed_version):
+    pass
+
+
+migrate = up
