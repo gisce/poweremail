c0e2554285fc7a3c574ba07d40a965eb7d0703f2
hperezgisce <hperez@gisce.net>
2026-03-20 10:07:45 +0100

    uptdate migration script

--- migrations/5.0.26.5.0/post-0001_add_last_date_and_send_count.py
+++ migrations/5.0.26.5.0/post-0001_add_last_date_and_send_count.py
@@ -1,5 +1,6 @@
 from tools import config
 from oopgrade.oopgrade import MigrationHelper
+from tools.translate import trans_load
 
 
 def up(cursor, installed_version):
@@ -15,6 +16,8 @@ def up(cursor, installed_version):
         'poweremail_template_tree',
     ]
     helper.update_xml_records(xml_path=xml_path, update_record_ids=record_ids)
+    trans_load(cursor, '{}/poweremail/i18n/ca_ES.po'.format(config['addons_path']), 'ca_ES')
+    trans_load(cursor, '{}/poweremail/i18n/es_ES.po'.format(config['addons_path']), 'es_ES')
 
 
 def down(cursor, installed_version):
