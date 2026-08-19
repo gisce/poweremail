a138b738743b62c3b4adb3d52f2e9974399d75b2
hperezgisce <hperez@gisce.net>
2026-08-19 10:54:04 +0200

    add translations

--- migrations/5.0.26.12.0/post-0002_update_send_wizard_view.py
+++ migrations/5.0.26.12.0/post-0002_update_send_wizard_view.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from tools import config
 from oopgrade.oopgrade import MigrationHelper
+from tools.translate import trans_load
 
 
 def up(cursor, installed_version):
@@ -12,6 +13,8 @@ def up(cursor, installed_version):
         xml_path='poweremail_send_wizard.xml',
         update_record_ids=['poweremail_send_wizard_form']
     )
+    trans_load(cursor, '{}/{}/i18n/ca_ES.po'.format(config['addons_path'], 'poweremail'), 'ca_ES')
+    trans_load(cursor, '{}/{}/i18n/es_ES.po'.format(config['addons_path'], 'poweremail'), 'es_ES')
 
 
 def down(cursor, installed_version):
