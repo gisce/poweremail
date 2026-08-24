fbf4e992ed848d33cee8d2b3d7bb8762fd325b81
gyalxen69 <gyalxen01@gmail.com>
2026-08-24 15:48:20 +0200

    added translation script

--- migrations/5.0.26.12.0/post-0002_add_report_xml_view_email_templates.py
+++ migrations/5.0.26.12.0/post-0002_add_report_xml_view_email_templates.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from tools import config
+from tools.translate import trans_load
 from oopgrade.oopgrade import MigrationHelper
 
 def up(cursor, installed_version):
     if not installed_version or config.updating_all:
         return
 
-    helper = MigrationHelper(cursor, 'poweremail')
+    module = 'poweremail'
+    helper = MigrationHelper(cursor, module_name=module)
 
     helper.init_model('ir.actions.report.xml')
 
@@ -16,6 +18,10 @@ def up(cursor, installed_version):
             'act_report_xml_view_poweremail'
         ]
     )
+
+    # Translations
+    trans_load(cursor, '{}/{}/i18n/es_ES.po'.format(config['addons_path'], module), 'es_ES')
+    trans_load(cursor, '{}/{}/i18n/ca_ES.po'.format(config['addons_path'], module), 'ca_ES')
 
 def down(cursor, installed_version):
     pass
